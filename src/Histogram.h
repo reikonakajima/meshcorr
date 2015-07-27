e718650399be881ec1dfe888646e8affcf39a60b
Reiko Nakajima <reiko.nakajima@gmail.com>
2015-07-27 13:23:13 +0200

    fix error checking and throwing in Histogram

--- src/Histogram.h
+++ src/Histogram.h
@@ -2,6 +2,15 @@
 #ifndef HISTOGRAM_H
 #define HISTOGRAM_H
 
+
+class HistogramError : public MyException {
+ public:
+ HistogramError(const string& m="") :
+  MyException("HistogramError: " +m) {}
+};
+
+
+
 //
 // header
 //
@@ -11,17 +20,17 @@ class HistogramLogBin {
   // rmin, rmax must be normalized to box side
   double operator[](int i) const { 
     if (bin) return bin[i]; 
-    else throw NBodySimError("HistogramLogBin not set"); }
+    else throw HistogramError("HistogramLogBin not set"); }
   double rsq(int i) const {
     if (bin) return bin[i]*bin[i];
-    else throw NBodySimError("HistogramLogBin not set"); }
+    else throw HistogramError("HistogramLogBin not set"); }
   int size() const { return Nbin; }
   double getRangeMin() const { 
     if (bin) return bin[0]; 
-    else throw NBodySimError("HistogramLogBin not set"); }
+    else throw HistogramError("HistogramLogBin not set"); }
   double getRangeMax() const { 
     if (bin) return bin[Nbin]; 
-    else throw NBodySimError("HistogramLogBin not set"); }
+    else throw HistogramError("HistogramLogBin not set"); }
  private:
   int    Nbin;
   double* bin;
@@ -34,10 +43,12 @@ class HistogramLogBin {
 HistogramLogBin::HistogramLogBin(float rmin, float rmax, int nbin_) 
 : Nbin(nbin_) {
   if (rmin <= 0. || rmax <= 0.) 
-    throw NBodySimError("negative rmin or rmax");
+    throw HistogramError("negative or zero rmin or rmax");
+  /*  // only needed for periodic conditions
   if (rmin > 1. || rmax > 1.)
-    throw NBodySimError("rmin or rmax not normalized to box size");
-  
+    throw HistogramError("rmin or rmax not normalized to box size");
+  */
+
   bin = new double[Nbin+1];
   double logoffset = log(rmin);
   double logsize = log(rmax/rmin);
